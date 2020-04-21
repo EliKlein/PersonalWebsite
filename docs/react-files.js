7d4b055405c66dd184e1e8d7d37f40f2acb3dad6
Eli Klein <eskklein@comcast.net>
2020-04-21 10:36:54 -0700

    copied/updated react-files.js it lets me put in a command to add a component because adding .css and .test.js and all the repeated starting code is extra work

---
+++ docs/react-files.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+for (let i = 2; i < process.argv.length; i++) {
+  let base = process.argv[i];
+  // if (process.argv[i][0] === "-") {
+  //   throw new Error("There are no '-' arguments for this command");
+  // }
+
+  fs.appendFile(`src/${base}.js`, `import React from 'react';
+import './${base}.css';
+
+function ${base}({}) {
+  return (
+    <>
+    </>
+  )
+}
+
+export default ${base};`, function (err) {
+    console.log(err);
+  });
+
+  fs.appendFile(`tests/${base}.test.js`, `import React from 'react';
+import {render} from '@testing-library/react';
+import ${base} from './${base}'
+
+it('renders without crashing', function(){
+  render(<${base}/>);
+});
+
+it('matches snapshot', function(){
+  const {asFragment} = render(<${base}/>);
+  expect(asFragment()).toMatchSnapshot();
+});`, function (err) {
+    console.log(err);
+  });
+
+  fs.appendFile(`src/${base}.css`, `.${base}{
+  /* CSS HERE */
+}`, function (err) {
+    console.log(err);
+  });
+}
